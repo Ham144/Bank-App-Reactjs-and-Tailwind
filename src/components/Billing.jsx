8d362547cec07636f468ab05dcb39bcdb413cd1f
ham144 <24434muhammad.yafizham@gmail.com>
2024-01-30 02:26:23 +0700

    bussiness, billing done

--- src/components/Billing.jsx
+++ src/components/Billing.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import { apple, bill, google } from "../assets"
+import styles, {layout} from "../style"
 
-const Billing = () => {
-  return (
-    <div>Billing</div>
-  )
-}
+const Billing = () => (
+  
+    <section id="product" className={`${layout.sectionReverse} `}>
+        <div className={layout.sectionImgReverse}>
+          <img src={bill} alt="billing" className={`w-[100%] h-[100%] relative z-[5]`} />
+
+          <div className={`absolute z-[3] -left-1/2 top-0 w-[50%] h-[50%] rounded-full white__gradient`}/>
+          <div className={`absolute z-[1] -left-1/2 bottom-0 w-[50%] h-[50%] rounded-full pink__gradient`}/>
+
+        </div>
+        <div className={`${layout.sectionInfo} lg:ml-64`}>
+          <h2 className={styles.heading2}>Easily control your <br className={`sm:block hidden text-white`} />
+            billing & invoicing.
+          </h2>
+          <p className={`${styles.paragraph} max-w-[470px] mt-5 `}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione maiores mollitia recusandae molestias ab reiciendis fugit sunt voluptatem explicabo laudantium, ducimus in commodi quae. At sequi corrupti nulla quod quisquam maxime beatae maiores!</p>
+        </div>
+    </section>
+  
+)
 
 export default Billing
--- src/components/Bussiness.jsx
+++ src/components/Bussiness.jsx
@@ -1,8 +1,39 @@
-import React from 'react'
+import styles, { layout } from '../style'
+import {features} from '../constants'
+import Button from './Button'
+
+const FeaturedCard = ({icon, title, content, index}) => (
+  <div className={`flex flex-row p-6 rounded-[20px] ${index !== features.length  -1 ? "mb-6"  : "mb-0"} feature-card`}>
+    <div className={`w-[64px] h-[64px] rounded-full ${styles.flexCenter} bg-dimBlue`}>
+        <img src={icon} alt="icon" className={`w-[50%] h-[50%] object-contain`}/>
+    </div>
+    <div className='flex-1 flex-col ml-3' >
+      <h4 className='font-poppins font-semibold text-white text-[18px] leading-[23px] mb-1'>
+        {title}
+      </h4>
+      <p className='font-poppins font-normal text-white text-[16px] leading-[24px] mb-1'>{content}</p>
+    </div>
+  </div>
+)
 
 const Bussiness = () => {
+  
   return (
-    <div>Bussiness</div>
+    <section id={Bussiness.id} className={`layout.section`}>
+      <section className='flex ss:flex-row flex-col'>
+        <div className={layout.sectionInfo}>
+          <h2 className={styles.heading2}>You do the business <br className={`sm:block hidden`}/>
+           we'll handle the money.</h2>
+          <p className={`${styles.paragraph} max-w-[470px] mt-5`}>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Corporis voluptatum illo accusamus, optio, tenetur officiis incidunt quibusdam sunt ad veritatis provident esse corrupti.</p>
+          <Button styles="mt-10 rounded-full ">Get Started</Button>
+        </div>
+        <div className={`${layout.sectionImg} flex-col`}>
+          {features.map((feature, index) => (
+            <FeaturedCard key={feature.id} {...feature} index={index}/>
+          ))}
+        </div>
+      </section>
+    </section>
   )
 }
 
